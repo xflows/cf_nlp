e65f9bd783712f69a368c819a5bf193a2f4317a0
matejMartinc <matejmart@gmail.com>
2017-06-24 02:34:42 +0200

    clowdflows nlp package

---
+++ README.md
@@ -0,0 +1,25 @@
+# TextFlows Core Module #
+
+
+A [ClowdFlows](https://github.com/xflows/clowdflows/) package, which contains widgets for natural language processing. The package can also be used with [ClowdFlows](https://github.com/xflows/clowdflows/) 2.0.
+
+[![Documentation Status](https://readthedocs.org/projects/rdm/badge/?version=latest)](http://clowdflows.readthedocs.io/)
+
+Currently, the project contains components for different corpus operations, basic natural language processing operations such as tokenization, stop word removal, lemmatization, part-of-speech tagging, etc. It also has modules for tweet streaming, term extraction and gender classification.
+
+
+## Installation, documentation ##
+
+Please find installation instructions, examples and API reference on [Read the Docs](http://clowdflows.readthedocs.io/).
+
+## Note ##
+
+Please note that this is a research project and that drastic changes can be (and are) made pretty regularly. Changes are documented in the [CHANGELOG](CHANGELOG.md).
+
+Pull requests and issues are welcome.
+
+## Contributors to the cf_nlp package code ##
+
+Matej Martinc (@matejMartinc)
+
+* [Knowledge Technologies Department](http://kt.ijs.si), Jožef Stefan Institute, Ljubljana
---
+++ setup.py
@@ -0,0 +1,43 @@
+import os
+from setuptools import setup
+
+with open(os.path.join(os.path.dirname(__file__), 'README.md')) as readme:
+    README = readme.read()
+
+# allow setup.py to be run from any path
+os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
+
+setup(
+    name='cf_nlp',
+    version='0.0.1',
+    packages=['cf_nlp'],
+    include_package_data=True,
+    license='MIT License',
+    description='ClowdFlows natural language processing module',
+    long_description=README,
+    url='https://github.com/xflows/cf_nlp',
+    author='Matej Martinc',
+    author_email='matej.martinc@ijs.si',
+    classifiers=[
+        'Development Status :: 2 - Pre-Alpha',
+        'Environment :: Web Environment',
+        'Framework :: Django',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Topic :: Internet :: WWW/HTTP',
+        'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+        'Topic :: Scientific/Engineering :: Artificial Intelligence',
+        'Topic :: Software Development :: Libraries'
+    ],
+    install_requires=[
+        'pandas==0.20.1',
+        'langid==1.1.6',
+		'tweepy==3.5.0',
+		'marisa-trie==0.74',
+		'python-crfsuite==0.9',
+		'reldi==1.6'
+    ]
+)
